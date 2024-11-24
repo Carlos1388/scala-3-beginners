76cdae0422b9e50eb35e48db75c573129776d092
Carlos Pov <carlospovedat1388@gmail.com>
2024-11-24 22:43:58 +0100

    lesson 9 - OOP Exercises

--- src/main/scala/com/rockthejvm/part2oop/OOBasics.scala
+++ src/main/scala/com/rockthejvm/part2oop/OOBasics.scala
@@ -37,8 +37,52 @@ object OOBasics {
   val johnSaysHi = aPerson.greet()
 
 
+  // Exercise 1
+  class Writer(first: String, surname: String,val year: Int) {
+    def fullname(): String = {
+      s"$first $surname"
+    }
+  }
+
+  class Novel(name: String,yearOfRelease: Int,author: Writer) {
+    def authorAge(): Int = yearOfRelease - author.year
+    def isWrittenBy(author: Writer): Boolean = author == this.author
+    def copy(newEditionYear: Int): Novel = new Novel(name, newEditionYear, this.author)
+  }
+
+  /** Exercise 2: Immutable counter class
+   * - constructed with initial count
+   * - increment/decrement => new instance of counter every time, because we don't want to use variables
+   * - overload to increment(n) and decrement(n) => New instance of counter
+   * - print the current count
+  */
+
+  class Counter(count: Int = 0) {
+    def increment(): Counter = new Counter(count + 1)
+    def decrement(): Counter = if (count !=0) new Counter(count - 1) else this
+    def increment(n: Int): Counter = {
+      if (n <= 0) this
+      else increment().increment(n - 1)
+    }
+
+    def decrement(n: Int): Counter = {
+      if (n <= 0) this
+      else decrement().decrement(n - 1)
+    } // marked as tailrecursion but not really, if we test it then we'll get SO
+
+    def show_count(): Unit = println(s"Current user count is: $count")
+  }
+
+
+
   def main(args: Array[String]): Unit = {
-    println(johnSayHiToDaniel)
-    println(johnSaysHi)
+    //println(johnSayHiToDaniel)
+    //println(johnSaysHi)
+
+    val counter = new Counter()
+    counter.show_count()
+    counter.increment(8).show_count() // 8
+    counter.decrement().show_count() // 0
+    counter.increment(10).decrement(5).show_count() // 5
   }
 }
